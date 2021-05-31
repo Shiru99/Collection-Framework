64ed7b7a9069bc7b27f2305b1040a9d1d457966d
Shiru99 <shriram.ghadge1999@gmail.com>
2021-05-31 11:56:07 +0530

    Sets - LinkedHashSets(maintains order using linkedlist)

--- Collection/Sets/LinkedHashSets.java
+++ Collection/Sets/LinkedHashSets.java
@@ -2,27 +2,23 @@
 
     LinkedHashSet maintains the order of elements other features are same as HashSet
 
-    NO constant time benefits        // generally O(n)         // uses Linked List 
+    NO constant time benefits   // generally O(n)   // uses Linked List 
 
 */
 
-
-
 import java.util.*;
-
 
 public class LinkedHashSets {
     public static void main(String[] args) {
         
-                Set<String> ls = new LinkedHashSet<>();  
-        
+        Set<String> ls = new LinkedHashSet<>();  
 
-                System.out.println(ls.add("Mango"));     //  true
-                System.out.println(ls.add("Banana"));    //  true
-                System.out.println(ls.add("Apple"));     //  true
-                System.out.println(ls.add("Mango"));     //  false
-        
-                System.out.println(ls);                  //  [Mango, Banana, Apple]   // EXACT order
+        System.out.println(ls.add("Mango"));     //  true
+        System.out.println(ls.add("Banana"));    //  true
+        System.out.println(ls.add("Apple"));     //  true
+        System.out.println(ls.add("Mango"));     //  false
+
+        System.out.println(ls);                  //  [Mango, Banana, Apple]   // EXACT order
         
     }
 }
