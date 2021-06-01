905b981fe2837ea5c33c2d0aa558ff6131bbd7b5
Shiru99 <shriram.ghadge1999@gmail.com>
2021-06-01 08:51:34 +0530

    Comparable<Obj>

--- SortingTechniques/ComparableObjects.java
+++ SortingTechniques/ComparableObjects.java
@@ -1,10 +1,7 @@
-package DSA.CollectionFramework.SortingTechniques;
-
-
 import java.util.*;
 
-public class ComparableObjects {
-    
+public class ComparableObjects 
+{    
     public static void main(String[] args) {
         
         List<Student> students = new ArrayList<>();
@@ -15,84 +12,57 @@ public class ComparableObjects {
         students.add(new Student(30, "Tom"));
         students.add(new Student(37, "Cat"));
 
+        /*  Student [marks=33, name=John]
+            Student [marks=33, name=Jen]
+            Student [marks=34, name=Jenny]
+            Student [marks=31, name=Jerry]
+            Student [marks=30, name=Tom]
+            Student [marks=37, name=Cat]    */
+
         Collections.sort(students);
+        students.forEach(System.out::println);      // lambda expression
 
-        // lambda expression :
-        students.forEach(System.out::println);
-
-            /* 
-            
-                before adding Comparables :
-
-                    Student [marks=33, name=John]
-                    Student [marks=33, name=Jen]
-                    Student [marks=34, name=Jenny]
-                    Student [marks=31, name=Jerry]
-                    Student [marks=30, name=Tom]
-                    Student [marks=37, name=Cat]
-            */
-
-            /* 
-            
-                After adding Comparables :
-
-                    Student [marks=37, name=Cat]
-                    Student [marks=34, name=Jenny]
-                    Student [marks=33, name=John]
-                    Student [marks=33, name=Jen]
-                    Student [marks=31, name=Jerry]
-                    Student [marks=30, name=Tom]
-            */
-
+        /*  After adding Comparables :
+    
+                Student [marks=30, name=Tom]
+                Student [marks=31, name=Jerry]
+                Student [marks=33, name=Jen]
+                Student [marks=33, name=John]
+                Student [marks=34, name=Jenny]
+                Student [marks=37, name=Cat]    */
     }
 }
 
 
 
-class Student implements Comparable<Student>{
+class Student implements Comparable<Student>
+{
     int marks;
     String name;
-
-    @Override
-    public String toString() {
-        return "Student [marks=" + marks + ", name=" + name + "]";
-    }
 
     public Student(int marks,String name){
         this.marks = marks;
         this.name = name;
     }
 
-    public int getMarks() {
-        return marks;
-    }
-
-    public void setMarks(int marks) {
-        this.marks = marks;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public String toString() {
+        return "Student [marks=" + marks + ", name=" + name + "]";
     }
 
     @Override
-    public int compareTo(Student obj) {
-        //  For Decreasing Order :
-        //      if this returns +ve num ==> current obj smaller
-        //                      -ve     ==> current obj Bigger
-        //                      0       ==> current obj Equal
+    public int compareTo(Student that) 
+    {
+        /*  returns : (By Default Increasing Order)
 
-        if(this.marks > obj.marks) return -1;
-        if(this.marks < obj.marks) return 1;
-        return this.name.compareTo(obj.name);       // in case of marks equal ==> sorts by names
-                                                    // string sorting is already implemented 
+                +ve     ==> this obj is Bigger
+                -ve     ==> this obj is smaller
+                0       ==> Equal           
+        */                     
+
+        if(this.marks > that.marks) return +1;
+        if(this.marks < that.marks) return -1;
+        return this.name.compareTo(that.name);       
+        // in case of marks equal ==> sorts by names string sorting is already implemented 
     }
-
-    
-    
-
 }
