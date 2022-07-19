b48b06e6e0c79a9243ae4d7ee774f3d72f961269
Shiru99 <shriram.ghadge1999@gmail.com>
2022-07-19 11:05:38 +0530

    Map - TreeMap (Sorted Map)

--- Maps/TreeMaps.java
+++ Maps/TreeMaps.java
@@ -1,3 +1,23 @@
+package Maps;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
 public class TreeMaps {
+    public static void main(String[] args) {
+        //  Sorted Map
+        Map<Integer,String> treeMap = new TreeMap<Integer,String>(); 
+        treeMap.put(3,"Three");
+        treeMap.put(4,"Four");
+        treeMap.put(1,"One");
+        treeMap.put(2,"Two");
+
+        for (Entry<Integer,String> entry : treeMap.entrySet()) {
+            System.out.println(entry.getKey()+" - "+entry.getValue());
+            // 1 - One, 2 - Two, 3 - Three, 4 - Four
+        }
+
+    }   
     
 }
