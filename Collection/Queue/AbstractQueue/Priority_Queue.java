6dea9d238a43aac1578a90f0fecc634fc924f798
Shiru99 <shriram.ghadge1999@gmail.com>
2022-07-19 10:20:56 +0530

    Queue [PriorityQueue]

--- Collection/Queue/AbstractQueue/Priority_Queue.java
+++ Collection/Queue/AbstractQueue/Priority_Queue.java
@@ -1,3 +1,4 @@
+package Collection.Queue.AbstractQueue;
 
 /* 
 
@@ -5,7 +6,10 @@
 
 */
 
-import java.util.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Collections;
+
 
 public class Priority_Queue {
     public static void main(String args[]) 
